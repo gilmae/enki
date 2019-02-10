fd7331f3d98e10a3e76a5cbf62c9d4558ccf24cf
gilmae <avocadia@fastmail.fm>
2019-02-10 21:27:27 +1100

    Adopt Twitter and Mastodon syncs to new shared modules

--- notes/sync_to_mastodon.rb
+++ notes/sync_to_mastodon.rb
@@ -1,113 +1,57 @@
-require 'json'
-require 'open-uri'
-require 'mastodon'
-require 'tempfile'
-require 'uri'
-require 'http'
+require "mastodon"
 
-    def initialize_config
-        p "Initializing config"
-        config = {}
-        config[:next_point] = get_starting_point 'end'
-        config
-    end
+BASE_DIR = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 
-    def get_config
-        filename = File.expand_path(File.join("~", ".enki", "#{File.basename(__FILE__)}.config"))
-        return initialize_config unless File.exists?(filename)
-        
-        config = File.open(filename, "r") { |f| 
-            JSON.parse(f.readlines.join("\n"))
-        }
-        
-        return initialize_config unless config.class == {}.class
-    
-        config = config.each_with_object({}){|(k,v), h| h[k.to_sym] = v}
-        initialize_config unless config[:next_point]
-        config
-    end
-    
-    def store_config config
-        filename = File.expand_path(File.join("~", ".enki", "#{File.basename(__FILE__)}.config"))
-        File.open(filename, "w") { |f| 
-            f << config.to_json
-        }
-    end
-    
-    def get_points last
-        uri = "http://avocadia.net/stream/get_records/#{last}/10"
-    
-        open(uri) { |f|
-            JSON.parse(f.readlines.join("\n"))
-        }.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-    end
+require("#{BASE_DIR}/enki.rb")
+require("#{BASE_DIR}/enbilulu.rb")
 
-    def get_starting_point type, from = 0
-        uri = "http://avocadia.net/stream/get_stream_position/#{type}/#{from}"
-    
-        open(uri) { |f|
-            JSON.parse(f.readlines.join("\n"))
-        }["point"]
-    end
+CONFIG_NAME = "#{File.basename(__FILE__)}.config"
 
-    def save_to_tempfile(url)
-        uri = URI.parse(url)
-        name = uri.path.split("/").last
-        filepath = "/tmp/#{name}"
-        Net::HTTP.start(uri.host, uri.port) do |http|
-          resp = http.get(uri.path)
-          File.open(filepath, "wb") { |file| 
-            file.write(resp.body)
-          }
-        end
+config = Enki.get_config(CONFIG_NAME) {
+  {:next_point => Enbilulu.get_starting_point("end")}
+}
 
-        filepath
-      end
-
-config = get_config
-
-data = get_points config[:next_point]
+data = Enbilulu.get_points config[:next_point]
 working_on = config[:next_point]
 
 mastodon = Mastodon::REST::Client.new(
-    base_url: config[:mastodon]["base_url"], 
-    bearer_token: config[:mastodon]["bearer_token"]
+  base_url: config[:mastodon]["base_url"],
+  bearer_token: config[:mastodon]["bearer_token"],
 )
 
 begin
-    while (!data[:records].empty?)
-        data[:records].each{ |r| 
-            
-            working_on = r["sequence_number"] 
-            next unless (r["data"]["content"] && !r["data"]["name"])
+  while (!data[:records].empty?)
+    data[:records].each { |r|
+      working_on = r["sequence_number"]
+      next unless (r["data"]["content"] && !r["data"]["name"])
 
-            entry = r["data"].each_with_object({}){|(k,v), h| h[k.downcase] = v}
-            
-            created_at = DateTime.parse(r["created_at"])
-            
-            tweet = entry["content"]
-            photo_ids = []
-            if entry.key? "photo"
-                photos = [entry["photo"]].flatten.map{|ph| HTTP::FormData::File.new(save_to_tempfile(ph) ) }
-                
-                # Should be uploading media, but the mastodon gem keeps failing.
-                # So just adding the links on for now
-                 photo_ids = photos.map { |p|
-                     puts "Uploading file"
-                     mastodon.upload_media(p).id
-                 }
-                 puts "Syncing #{tweet}\n\twith media: #{photo_ids.join(", ")}"
-                 mastodon.create_status(tweet, nil, photo_ids)
-            else
-                puts "Syncing #{tweet}"
-                mastodon.create_status(tweet)
-            end
+      entry = r["data"].each_with_object({}) { |(k, v), h| h[k.downcase] = v }
 
-            config[:next_point] = working_on
+      created_at = DateTime.parse(r["created_at"])
+
+      tweet = entry["content"]
+      photo_ids = []
+      if entry.key? "photo"
+        photos = [entry["photo"]].flatten.map { |ph| HTTP::FormData::File.new(Enki.save_to_tempfile(ph)) }
+
+        # Should be uploading media, but the mastodon gem keeps failing.
+        # So just adding the links on for now
+        photo_ids = photos.map { |p|
+          puts "Uploading file"
+          mastodon.upload_media(p).id
         }
-        config[:next_point] = working_on + 1
-        data = get_points config[:next_point]
-    end
+        puts "Syncing #{tweet}\n\twith media: #{photo_ids.join(", ")}"
+        mastodon.create_status(tweet, nil, photo_ids)
+      else
+        puts "Syncing #{tweet}"
+        mastodon.create_status(tweet)
+      end
+
+      config[:next_point] = working_on
+    }
+    config[:next_point] = working_on + 1
+    data = Enbilulu.get_points config[:next_point]
+  end
 ensure
-    store_config config
+  Enki.store_config config, CONFIG_NAME
 end
--- notes/sync_to_twitter.rb
+++ notes/sync_to_twitter.rb
@@ -1,101 +1,61 @@
-require 'twitter'
-require 'json'
-require 'open-uri'
-require 'uri'
-require 'digest'
+require "twitter"
+require "uri"
 
-    def get_config
-        filename = File.expand_path(File.join("~", ".enki", "#{File.basename(__FILE__)}.config"))
-        return {:next_point=>0} unless File.exists?(filename)
-        
-        config = File.open(filename, "r") { |f| 
-            JSON.parse(f.readlines.join("\n"))
-        }
-        
-        return {:next_point=>0} unless config.class == {}.class
-    
-        config = config.each_with_object({}){|(k,v), h| h[k.to_sym] = v}
-        config[:next_point] = 0 unless config[:next_point]
-        config
-    end
-    
-    def store_config config
-        filename = File.expand_path(File.join("~", ".enki", "#{File.basename(__FILE__)}.config"))
-        File.open(filename, "w") { |f| 
-            f << config.to_json
-        }
-    end
-    
-    def get_points last
-        uri = "http://avocadia.net/stream/get_records/#{last}/10"
-    
-        open(uri) { |f|
-            JSON.parse(f.readlines.join("\n"))
-        }.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-    end
+BASE_DIR = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 
-    def save_to_tempfile(url)
-        uri = URI.parse(url)
-        name = uri.path.split("/").last
-        filepath = "/tmp/#{name}"
-        Net::HTTP.start(uri.host, uri.port) do |http|
-          resp = http.get(uri.path)
-          File.open(filepath, "wb") { |file| 
-            file.write(resp.body)
-          }
-        end
+require("#{BASE_DIR}/enki.rb")
+require("#{BASE_DIR}/enbilulu.rb")
 
-        filepath
-      end
+CONFIG_NAME = "#{File.basename(__FILE__)}.config"
 
-config = get_config
+config = Enki.get_config(CONFIG_NAME) {
+  {:next_point => Enbilulu.get_starting_point("end")}
+}
 
-data = get_points config[:next_point]
+data = Enbilulu.get_points config[:next_point]
 working_on = config[:next_point]
 
 twitter = Twitter::REST::Client.new do |tw|
-    tw.consumer_key = config[:twitter]["CONSUMER_KEY"]
-    tw.consumer_secret = config[:twitter]["CONSUMER_SECRET"]
-    tw.access_token = config[:twitter]["OAUTH_TOKEN"]
-    tw.access_token_secret = config[:twitter]["OAUTH_TOKEN_SECRET"]
-  end
-
+  tw.consumer_key = config[:twitter]["CONSUMER_KEY"]
+  tw.consumer_secret = config[:twitter]["CONSUMER_SECRET"]
+  tw.access_token = config[:twitter]["OAUTH_TOKEN"]
+  tw.access_token_secret = config[:twitter]["OAUTH_TOKEN_SECRET"]
+end
 
 begin
-    while (!data[:records].empty?)
-        data[:records].each{ |r| 
-            
-            working_on = r["sequence_number"] 
-            next unless (r["data"]["content"] && !r["data"]["name"])
+  while (!data[:records].empty?)
+    data[:records].each { |r|
+      working_on = r["sequence_number"]
+      next unless (r["data"]["content"] && !r["data"]["name"])
 
-            entry = r["data"].each_with_object({}){|(k,v), h| h[k.downcase] = v}
-            
-            created_at = DateTime.parse(r["created_at"])
-            
-            tweet = entry["content"]
-            photos = []
-            if entry.key? "photo"
-                photos = [entry["photo"]].flatten.map{|ph| File.new(save_to_tempfile(ph))}
-            end
+      entry = r["data"].each_with_object({}) { |(k, v), h| h[k.downcase] = v }
 
-            if photos.empty?
-                puts "Syncing #{tweet}"
+      created_at = DateTime.parse(r["created_at"])
 
-                twitter.update(tweet)
-            else
-                puts "Syncing #{tweet} with #{photos.length} images"
-                photos.each { |f| 
-                    puts f.path
-                } 
-                
-                twitter.update_with_media(tweet, photos) if !photos.empty?
-            end
+      tweet = entry["content"]
+      photos = []
+      if entry.key? "photo"
+        photos = [entry["photo"]].flatten.map { |ph| File.new(Enki.save_to_tempfile(ph)) }
+      end
 
-            config[:next_point] = working_on
+      if photos.empty?
+        puts "Syncing #{tweet}"
+
+        twitter.update(tweet)
+      else
+        puts "Syncing #{tweet} with #{photos.length} images"
+        photos.each { |f|
+          puts f.path
         }
-        config[:next_point] = working_on + 1
-        data = get_points config[:next_point]
-    end
+
+        twitter.update_with_media(tweet, photos) if !photos.empty?
+      end
+
+      config[:next_point] = working_on
+    }
+    config[:next_point] = working_on + 1
+    data = Enbilulu.get_points config[:next_point]
+  end
 ensure
-    store_config config
+  Enki.store_config config, CONFIG_NAME
 end
