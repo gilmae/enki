77b20260edd69020e779bd5c5b2b0b1445d329a9
gilmae <avocadia@fastmail.fm>
2019-02-10 21:44:34 +1100

    Intergrate pinboard sync with new shared modulkes

--- bookmarks/sync_to_pinboard.rb
+++ bookmarks/sync_to_pinboard.rb
@@ -1,78 +1,48 @@
-require 'json'
-require 'open-uri'
-require './pinboard'
-require 'time'
+require "time"
+
+BASE_DIR = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+
+require("#{BASE_DIR}/enki.rb")
+require("#{BASE_DIR}/enbilulu.rb")
+require("#{File.expand_path(File.dirname(__FILE__))}/pinboard")
 
 include Pinboard
 
+CONFIG_NAME = "#{File.basename(__FILE__)}.config"
 
-def get_config
-    filename = File.expand_path(File.join("~", ".enki", "#{File.basename(__FILE__)}.config"))
-    
-    return {:next_point=>0} unless File.exists?(filename)
-    
-    config = File.open(filename, "r") { |f| 
-        JSON.parse(f.readlines.join("\n"))
-    }
-    
-    return {:next_point=>0} unless config.class == {}.class
+config = Enki.get_config(CONFIG_NAME) {
+  {:next_point => 0}
+}
 
-    config = config.each_with_object({}){|(k,v), h| h[k.to_sym] = v}
-    config[:next_point] = 0 unless config[:next_point]
-    config
-end
-
-def store_config config
-    filename = File.expand_path(File.join("~", ".enki", "#{File.basename(__FILE__)}.config"))
-    File.open(filename, "w") { |f| 
-        f << config.to_json
-    }
-end
-
-def get_points last
-    uri = "http://avocadia.net/stream/get_records/#{last}/10"
-
-    open(uri) { |f|
-        JSON.parse(f.readlines.join("\n"))
-    }.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-end
-
-def is_number? string
-    true if Float(string) rescue false
-end
-
-config = get_config
-
-data = get_points config[:next_point]
+data = Enbilulu.get_points config[:next_point]
 working_on = config[:next_point]
+
 begin
-    while (!data[:records].empty?)
-        data[:records].each{ |r| 
-            
-            working_on = r["sequence_number"] 
-            next unless r["data"]["bookmark-of"]
+  while (!data[:records].empty?)
+    data[:records].each { |r|
+      working_on = r["sequence_number"]
+      next unless r["data"]["bookmark-of"]
 
-            entry = r["data"].each_with_object({}){|(k,v), h| h[k.downcase] = v}
-            created_at = DateTime.parse(r["created_at"])
-            categories = entry["category"]
-            categories = categories.split("\s") unless (categories.is_a? Array)
-            categories = categories.join(",") unless (categories.is_a? String)
+      entry = r["data"].each_with_object({}) { |(k, v), h| h[k.downcase] = v }
+      created_at = DateTime.parse(r["created_at"])
+      categories = entry["category"]
+      categories = categories.split("\s") unless (categories.is_a? Array)
+      categories = categories.join(",") unless (categories.is_a? String)
 
-            bookmark = {
-                "url"=>entry["bookmark-of"],
-                "description"=>entry["name"],
-                "extended"=>entry["content"],
-                "tags"=>categories,
-                "dt"=>created_at.new_offset(0).strftime("%FT%TZ")
-            }
-            puts "Syncing #{bookmark}"
-            Pinboard::add bookmark, config[:pinboard]["username"], config[:pinboard]["password"]
-            config[:next_point] = working_on
-        }
-        config[:next_point] = working_on + 1
-        data = get_points config[:next_point]
-    end
+      bookmark = {
+        "url" => entry["bookmark-of"],
+        "description" => entry["name"],
+        "extended" => entry["content"],
+        "tags" => categories,
+        "dt" => created_at.new_offset(0).strftime("%FT%TZ"),
+      }
+      puts "Syncing #{bookmark}"
+      Pinboard::add bookmark, config[:pinboard]["username"], config[:pinboard]["password"]
+      config[:next_point] = working_on
+    }
+    config[:next_point] = working_on + 1
+    data = Enbilulu.get_points config[:next_point]
+  end
 ensure
-    store_config config
+  Enki.store_config config, CONFIG_NAME
 end
-
